5755c3a0e94156cf1519a9f0784ca017b66aac59
IDNT <baa632762@gmail.com>
2026-04-11 20:33:00 +0800

    Add logging to database connection in get_db
    Refactor get_db function to include database connection logging.

---
+++ database.py
@@ -0,0 +1,28 @@
+# 数据库连接、Base、engine
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+'''
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+'''
+
+def get_db():
+    db = SessionLocal()
+    print("DB CONNECT:", db.bind.url)
+    yield db
+    db.close()
